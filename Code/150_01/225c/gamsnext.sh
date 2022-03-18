2be25be669cf4b2f28cb020b69476fbdd79542f0
Charlotte Ritter <mpwm031h@woody3.rrze.uni-erlangen.de>
2022-03-18 10:01:14 +0100

    To change: Alg output

--- Code/150_01/225c/gamsnext.sh
+++
@@ -1,1 +0,0 @@
-gmsgenus.run /home/hpc/mpwm/mpwm031h/ArticleSubgradient/Code/150_01/225c/ /home/hpc/mpwm/mpwm031h/ArticleSubgradient/Code/150_01/ $1 /home/hpc/mpwm/mpwm031h/ArticleSubgradient/Code/150_01/225c/gamscntr.dat /home/woody/mpwm/shared/software/gams24.8_linux_x64_64_sfx/ GUROBI
